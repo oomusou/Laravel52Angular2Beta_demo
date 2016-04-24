5440b9fe55dab96d28888986ff2a58b0ed9c4c7e
oomusou <oomusou@gmail.com>
2016-04-24 21:07:46 +0800

    設定 Laravel Elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var elixir = require('laravel-elixir');
+var elixirTypscript = require('elixir-typescript');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
+elixir(function (mix) {
     mix.sass('app.scss');
+
+    mix.copy('node_modules/angular2', 'public/angular2')
+        .copy('node_modules/rxjs', 'public/rxjs')
+        .copy('node_modules/systemjs', 'public/systemjs')
+        .copy('node_modules/es6-promise', 'public/es6-promise')
+        .copy('node_modules/es6-shim', 'public/es6-shim')
+        .copy('node_modules/zone.js', 'public/zone.js');
+
+    mix.typescript([
+            'app.component.ts',
+            'main.ts'
+        ],
+        'public/'
+    );
+
+    mix.browserSync({
+        files: ['app/**/*', 'public/**/*', 'resources/views/**/*'],
+        port: 5000,
+        proxy: 'localhost:8000'
+    });
 });
