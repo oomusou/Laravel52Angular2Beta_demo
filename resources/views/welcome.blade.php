0540a11a83c81f01de1ccda2f5ef36ebf35d2a3b
oomusou <oomusou@gmail.com>
2016-04-24 20:01:47 +0800

    將 Angular 2 整合進 Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,30 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+        <!-- 1. Load libraries -->
+        <script src="es6-shim/es6-shim.min.js"></script>
+        <script src="systemjs/dist/system-polyfills.js"></script>
+        <script src="angular2/bundles/angular2-polyfills.js"></script>
+        <script src="systemjs/dist/system.src.js"></script>
+        <script src="rxjs/bundles/Rx.js"></script>
+        <script src="angular2/bundles/angular2.dev.js"></script>
+
+        <!-- 2. Configure SystemJS -->
+        <script>
+            System.config({
+                "defaultJSExtensions": true,
+                packages: {
+                    app: {
+                        format: 'register',
+                        defaultExtension: 'js'
+                    }
+                }
+            });
+
+            System.import('main')
+                    .then(null, console.error.bind(console));
+        </script>
+
         <style>
             html, body {
                 height: 100%;
@@ -39,6 +63,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <my-app>Loading...</my-app>
             </div>
         </div>
     </body>
